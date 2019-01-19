5392796e3fc79324dccf3d5d30be6120b7739649
ibbi20131 <44727739+ibbi20131@users.noreply.github.com>
2019-01-19 11:13:21 +0400

    Add files via upload

---
+++ facepics.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import time
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+def TakeSnapshotAndSave(pics):
+    # access the webcam (every webcam has a number, the default is 0)
+    cap = cv2.VideoCapture(0)
+
+    num = 0
+    while num < pics + 1:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # to detect faces in video
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+        x = 0
+        y = 20
+        
+
+        cv2.imwrite('opencv'+str(num)+'.jpg',frame)
+        num = num+1
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
